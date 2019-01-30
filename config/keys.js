20f340904ffa7e31aabd32717e0c50ce10bbad70
Nathan Saunders <nathansaunders@Peters-MBP-3.localdomain>
2019-01-30 12:20:24 +0000

    Added login and register routes with JWT auth using Passport

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb://devNathan:F3dd3**07mla@ds125392.mlab.com:25392/dev-connector-2019"
+    "mongodb://devNathan:F3dd3**07mla@ds125392.mlab.com:25392/dev-connector-2019",
+  secretOrKey: "secret"
 };
