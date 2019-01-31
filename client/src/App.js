c7e2f88ac21342374f8c52fdfa500bef35102c86
Nathan Saunders <nathan_saunders@hotmail.co.uk>
2019-01-31 17:35:28 +0000

    Added redux to manage authentication state for login and register

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser } from "./actions/authActions";
+import { Provider } from "react-redux";
 import "./App.css";
+
+import store from "./store";
 
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
@@ -8,20 +14,32 @@ import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 
+// Check for token
+if (localStorage.jwtToken) {
+  // Set auth token header
+  setAuthToken(localStorage.jwtToken);
+  // Decode token and get user info
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+}
+
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Route path="/" exact component={Landing} />
-          <div className="container">
-            <Route path="/register" exact component={Register} />
-            <Route path="/login" exact component={Login} />
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route path="/" exact component={Landing} />
+            <div className="container">
+              <Route path="/register" exact component={Register} />
+              <Route path="/login" exact component={Login} />
+            </div>
+            <Footer />
           </div>
-          <Footer />
-        </div>
-      </Router>
+        </Router>
+      </Provider>
     );
   }
 }
